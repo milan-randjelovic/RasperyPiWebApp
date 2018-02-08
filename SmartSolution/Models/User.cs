0a06a304d2a1650831e8c32e8e0052b913150414
stefan.pavlovic <spstefan5@gmail.com>
2018-02-08 12:13:21 +0100

    User model added

---
+++ SmartSolution/Models/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebPortal.Models
+{
+    public class User
+    {
+        [DataType(DataType.Text)]
+        public string Id { get; set; }
+        [DataType(DataType.Text)]
+        public string Name { get; set; }
+        [DataType(DataType.Text)]
+        public string Lastname { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [DataType(DataType.Text)]
+        public string Username { get; set; }
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public User()
+        {
+            this.Id = Guid.NewGuid().ToString();
+            this.Name = "";
+            this.Lastname = "";
+            this.Email = "";
+            this.Username = "";
+            this.Password = "";
+        }
+       
+
+    }
+}
